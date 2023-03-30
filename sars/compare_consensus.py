c98d2b66f682696d914fe88aba92918b7d61421d
Rodrigo Eusébio <rodrigojeusebio@gmail.com>
2023-03-30 07:00:06 +0100

    add debug option for consensus

--- sars/compare_consensus.py
+++ sars/compare_consensus.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from Bio.SeqIO.FastaIO import SimpleFastaParser
 from Bio.SeqIO import SeqRecord
+import sys
 
 
 def get_consensus(path) -> dict[str, str]:
@@ -13,10 +14,21 @@ def get_consensus(path) -> dict[str, str]:
 
 
 if __name__ == "__main__":
+    try:
+        debug = sys.argv[1]
+    except:
+        debug = False
     website = get_consensus("./website/AllConsensus.fasta")
-    print("website done")
     snakemake = get_consensus("./snakemake/AllConsensus.fasta")
-    print("snakemake done")
+    total_website = len(website.keys())
+    total_snakemake = len(snakemake.keys())
+    print(f"website: {total_website} | snakemake: {total_snakemake}")
+    total = len(website.keys())
+    count = 0
     for key in website:
         if website.get(key) == snakemake.get(key):
-            print(key)
+            count += 1
+        else:
+            if debug:
+                print(key)
+    print(f"There is {count} samples that are exactly equal out of {total}")
